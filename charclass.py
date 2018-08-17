d3e734180b192d5f42ed844bc2979fb5790a0bbe
jondsonline <32865855+jondsonline@users.noreply.github.com>
2018-08-17 18:24:31 -0500

    Finished game
    Added existing files to repository

---
+++ charclass.py
@@ -0,0 +1,90 @@
+import random
+
+class Creature():
+    def __init__(self, hp, ac, dam):
+        self.max_hit_points = hp
+        self.hit_points = hp
+        self.armor_class = ac
+        self.attack_damage = dam
+        self.alive = True
+
+    def take_damage(self, dam):
+        self.hit_points -= dam
+        if self.hit_points < 1:
+            self.alive = False
+
+    def attack_roll(self):
+        roll = random.randint(1, 10)
+        return roll
+
+    def to_hit(self, target_ac):
+        if self.attack_roll() >= target_ac:
+            return True
+        else:
+            return False
+
+    def damage_roll(self):
+        roll = random.randint(1, self.attack_damage)
+        return roll
+
+
+
+class Player(Creature):
+    def __init__(self, hp, ac, dam):
+        super().__init__(hp, ac, dam)
+        self.healing = 5
+        self.level = 1
+
+    def heal(self):
+        heal_chance = random.randint(1, 5)
+
+        if heal_chance == 5:
+            print("The healing spell backfires! You take 1 point of damage!")
+            self.take_damage(1)
+        else:
+            self.hit_points += heal_chance
+            if self.hit_points > self.max_hit_points:
+                self.hit_points = self.max_hit_points
+            print("Your healing spell heals {} hit point(s).".format(heal_chance))
+
+    def level_up(self):
+        self.level += 1
+        print("\nYou are now at level {}".format(self.level))
+
+        increased_stat = random.randint(1, 10)
+
+        if increased_stat >= 1 or increased_stat <= 5:
+            hp_increase = random.randint(2, 5)
+            self.max_hit_points += hp_increase
+            self.hit_points = self.max_hit_points
+            print("Your maximum hit points increases by {}!".format(hp_increase))
+        elif increased_stat >= 6 or increased_stat <= 8:
+            self.attack_damage += 1
+            print("Your attack damage increases by 1!")
+        elif increased_stat == 9:
+            self.healing += 1
+            print("Your healing skill increases by 1!")
+        elif increased_stat == 10:
+            if self.armor_class < 8:
+                self.armor_class += 1
+                print("Your armor class increases by 1!")
+            else:
+                self.max_hit_points += 6
+                self.hit_points = self.max_hit_points
+                print("Your maximum hit points increases by 6!")
+
+class Monster(Creature):
+    def __init__(self, hp, ac, dam):
+        super().__init__(hp, ac, dam)
+        self.level = 1
+
+    def level_up(self):
+        self.level += 1
+        self.alive = True
+
+        self.max_hit_points += random.randint(2, 4)
+        self.hit_points = self.max_hit_points
+        attack_increase_chance = random.randint(1, 2)
+        if attack_increase_chance == 1:
+            self.attack_damage += 1
+
---
+++ rpg-battle-2.py
@@ -0,0 +1,116 @@
+# rpg-battle.py
+#
+# A simple game involving a rpg battle
+# between the player and a monster.
+
+from charclass import *
+
+pc = Player(20, 6, 6)
+monster = Monster(12, 7, 5)
+
+def player_attacks():
+    player_hits = pc.to_hit(monster.armor_class)
+
+    if player_hits == True:
+        damage = pc.damage_roll()
+
+        print("You hit the monster for {} point(s) of damage.".format(damage))
+
+        monster.take_damage(damage)
+    else:
+        print("You miss!")
+
+
+def monster_attacks():
+    monster_hits = monster.to_hit(pc.armor_class)
+
+    if monster_hits == True:
+        damage = monster.damage_roll()
+
+        print("The monster hits you for {} point(s) of damage.".format(damage))
+
+        pc.take_damage(damage)
+    else:
+        print("The monster misses!")
+
+
+def pc_still_alive():
+    if not pc.alive:
+        print("You are killed!")
+        return False
+    else:
+        return True
+
+
+# THE GAME ROUTINE
+
+game_continues = True
+
+while game_continues:
+
+    combat_continues = True
+    combat_round = 0
+    player_levels_up = True
+
+    while combat_continues:
+        combat_round += 1
+        print("\nLevel {} - Round {}".format(pc.level, combat_round))
+        print("-----------------------------------------------------")
+        print("Player: HP = {}/{}, AC = {}, Damage = 1d{}, Heal = 1d{}".format(pc.hit_points, pc.max_hit_points,
+                                                                  pc.armor_class, pc.attack_damage, pc.healing-1))
+        print("Monster: HP = {}/{}, AC = {}, Damage = 1d{}".format(monster.hit_points, monster.max_hit_points,
+                                                                   monster.armor_class, monster.attack_damage))
+        print("-----------------------------------------------------")
+
+        action = input("Do you want to (A)ttack, (H)eal, or (R)un? ").lower()
+
+        print()
+
+        if action == "a":  # attack
+            player_attacks()
+
+            if not monster.alive:
+                print("You kill the monster!")
+                combat_continues = False
+            else:
+                monster_attacks()
+                combat_continues = pc_still_alive()
+
+        elif action == "h":
+            pc.heal()
+            if not pc_still_alive():
+
+                combat_continues = False
+            else:
+                monster_attacks()
+
+            combat_continues = pc_still_alive()
+
+        elif action == "r":
+            print("You attempt to flee the battle!")
+            monster_attacks()
+
+            if pc_still_alive():
+                print("You successfully escape and the battle is over!")
+
+            player_levels_up = False
+            combat_continues = False
+
+        else:
+            print("I don't recognize that command.")
+            combat_round -= 1
+
+    if not pc.alive:
+        game_continues = False
+    else:
+
+        play_again = input("\nDo you want to play again (Y/N)? ")
+
+        if play_again.upper() != "N":
+            monster.level_up()
+            pc.hit_points = pc.max_hit_points
+            if player_levels_up:
+                pc.level_up()
+        else:
+            print("Thanks for playing!")
+            game_continues = False
